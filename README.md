865bfb3dda3c919c282b2ed48ba3c835be0b73bf
tamzid620 <tamzid620@gmail.com>
2026-01-02 23:10:07 +0600

    priview section updated

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
  sample link: https://demo.harutheme.com/famipress/familypress-tree/?root_id=175
  
- => Live Site Link: https://azahar-mia-family.netlify.app/ https://azahar-mia.vercel.app/
+ => Live Site Link: https://azahar-mia-family.netlify.app/ https://azahar-mia-family-tree.vercel.app/
 
 • Html, Css, JavaScript,
 • Node.Js tailwind
--- src/components/Pages/Home/Gallary/Gallary.jsx
+++ src/components/Pages/Home/Gallary/Gallary.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 const Gallary = () => {
   const [likeCount, setLikeCount] = useState(158);
   const [isLiked, setIsLiked] = useState(false);
+  // const [views, setViews] = useState(0);
+
+  // useEffect(() => {
+  //   fetch("https://azahar-mia-family-tree.vercel.app/")
+  //     .then(res => res.json())
+  //     .then(data => setViews(data.value));
+  // }, []);
 
   const handleLikeClick = () => {
     if (!isLiked) {
@@ -26,34 +33,34 @@ const Gallary = () => {
       <div className="divider"></div>
 
       <div className="flex justify-center">
-  <div className="stats shadow font-semibold">
-  
-  <div  onClick={handleLikeClick} className="stat bg-gray-200 hover:bg-gray-300">
-    <div className="stat-figure text-secondary ">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    </div>
-    <div className="stat-title" disabled={isLiked}>{isLiked ? "Liked" : "Like"}</div>
-    <div 
-    className="font-extrabold text-4xl">{likeCount}</div>
-  </div>
-  
-  <div className="stat bg-slate-200 hover:bg-slate-300 ">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
-    </div>
-    <div className="stat-title">Total Views</div>
-    <div className="stat-value">320</div>
-  </div>
-  
-  <div className="stat bg-zinc-200 hover:bg-zinc-300 ">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
-    </div>
-    <div className="stat-title">Total Members</div>
-    <div className="stat-value">162</div>
-  </div>
-  
-</div>
+        <div className="stats shadow font-semibold">
+
+          <div onClick={handleLikeClick} className="stat bg-gray-200 hover:bg-gray-300">
+            <div className="stat-figure text-secondary ">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </div>
+            <div className="stat-title" disabled={isLiked}>{isLiked ? "Liked" : "Like"}</div>
+            <div
+              className="font-extrabold text-4xl">{likeCount}</div>
+          </div>
+
+          <div className="stat bg-slate-200 hover:bg-slate-300 ">
+            <div className="stat-figure text-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+            </div>
+            <div className="stat-title">Total Views</div>
+            <div className="stat-value">430</div>
+          </div>
+
+          <div className="stat bg-zinc-200 hover:bg-zinc-300 ">
+            <div className="stat-figure text-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+            </div>
+            <div className="stat-title">Total Members</div>
+            <div className="stat-value">162</div>
+          </div>
+
+        </div>
       </div>
 
     </div>
